4c19b7eb349a669b64e645c14fe065644db3c20f
Syed Mohammed Ismail <mohammedismail.dev@gmail.com>
2026-03-08 20:54:40 +0530

    Refactor double linked list implementation to use raw pointers, simplifying memory management and improving code clarity

--- linkedlist/Doublelinkedlist/double.cpp
+++ linkedlist/Doublelinkedlist/double.cpp
@@ -1,86 +1,74 @@
 #include<iostream>
-#include<memory>
 using namespace std;
 class node{
-   public:
+    public:
     int data;
-    node* prev;
-    unique_ptr<node> next;
-    node(int data, node* prev = nullptr, unique_ptr<node> next = nullptr)
-    {
+    node* previous;
+    node* next;
+    node(int data){
         this->data = data;
-        this->prev = prev;
-        this->next = move(next);
+        previous = nullptr;
+        next = nullptr;
+
     }
 };
 
 class doublelist{
     private:
-    unique_ptr<node> Head;
+    node* Head;
     node* Tail;
-   
-
-public:
+    public:
     doublelist(){
-        Head = nullptr;
-        Tail = nullptr;
+        Head= nullptr;
+        Tail= nullptr;
     }
-
     void insert(int data){
-       auto temp = make_unique<node>(data);
-       node* prevnode = temp.get();
-        if (Head == nullptr && Tail == nullptr)
+        node* temp = new node(data);
+        if (Head == nullptr)
         {
-            Tail = prevnode;
-            Head = move(temp);
-            cout<<"temptail : "<<Tail<<endl;
+            Head= temp;
+            Tail = temp;
             return;
         }
-        temp->next = move(temp);
-        temp->next->prev = prevnode;
-        Head = move(temp);
+        Head->previous = temp;
+        temp->next = Head;
+        Head= temp;
 
+        
     }
-   
     void print(){
-        node *helper = Head.get();
+        node* helper= Head;
         while (helper != nullptr)
         {
-            cout<<helper->data<<" ";
-            helper = helper->next.get();
+         cout<<helper->data<<" ";
+         helper = helper->next;
         }
-        
+        cout<<endl;
+    }
+    void Rprint(){
+        node* helper = Tail;
+        while (helper != nullptr)
+        {
+            cout << helper->data << " ";
+            helper = helper->previous;
+        }
+        cout<<endl;
     }
 
-    void Rprint(){
-       node* helper = Tail;
-       cout<<"tail: "<<helper<<endl;
-        while (helper != nullptr)
-        {
-            cout<<helper->data<<" ";
-            helper = helper->prev;
-        }
-        
-    }
 
 };
 
+
 int main(){
+    doublelist test;
+    for (size_t i = 1; i <= 5; i++)
     {
-        doublelist test;
-        for (size_t i = 1; i <=5; i++)
-        {
-            test.insert(i);
-        }
-        cout << "forward ";
-        test.print();
-        cout<<endl<<"reversed";
-        test.Rprint();
-
-
+        test.insert(i);
     }
-
-
-
-    return 0;
+    cout<<"current values : ";
+    test.print();
+    cout<<endl;
+    cout<<"reversed values : ";
+    test.Rprint();
+    cout<<endl;
 }
